9436824a89ee3949a74ead7557cdefcc88bb8e59
AfonsoMatElias <afonsomatumona@hotmail.com>
2022-05-16 19:41:04 +0100

    test: added content to lib-tester

--- lib-tester/index.js
+++ lib-tester/index.js
@@ -2,5 +2,53 @@ const { default: Validator } = require("@taikai/validator");
 
 (async () => {
 	const validator = new Validator({});
-	console.log(validator);	
+	const isValid1 = await validator.validate({
+		name: {
+			type: 'string',
+			options: {
+				presence: true
+			}
+		}
+	}, {});
+
+	// Validating Wrong input
+	console.log('#1 Attemp');
+	console.log('Is Valid Model: ', isValid1);
+	console.log('Validation Errors: ', validator.errors);
+
+	// Reseting error object
+	validator.reset();
+
+	const isValid2 = await validator.validate({
+		name: {
+			type: 'string',
+			options: {
+				presence: false
+			}
+		}
+	}, {});
+
+	// Validating Wrong input
+	console.log('#2 Attemp');
+	console.log('Is Valid Model: ', isValid2);
+	console.log('Validation Errors: ', validator.errors);
+
+	// Reseting error object
+	validator.reset();
+
+	const isValid3 = await validator.validate({
+		name: {
+			type: 'string',
+			options: {
+				presence: false
+			}
+		}
+	}, {
+		name: 'Afonso Matumona'
+	});
+
+	// Validating Wrong input
+	console.log('#3 Attemp');
+	console.log('Is Valid Model: ', isValid3);
+	console.log('Validation Errors: ', validator.errors);
 })();
