a7afced0725ac7097a5778c463afbcb851a30dcd
AfonsoMatElias <afonsomatumona@hotmail.com>
2022-05-16 19:39:56 +0100

    feat: specific types added

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,38 @@
+import Context from "../types/context";
+import ValidatorError from "../types/errors";
 import Options from "../types/options";
 import Schema from "../types/schema";
+import SchemaInput from "../types/schema-input";
+import SchemaOptions from "../types/schema-options";
+import SchemaType from "../types/schema-type";
 import { validateObj } from "./core/functions";
 
 class Validator {
-    errors = {};
-    ctx = {};
+    private ctx: Context;
 
-    constructor(ctx: any) {
-        this.ctx = ctx;        
+    errors: ValidatorError = {};
+
+    constructor(ctx: Context) {
+        this.ctx = ctx;   
     }
 
-    async validate(schema: Schema, data: any, options?: Options) {
+    async validate(schema: SchemaInput, data: object, options?: Options) {
         await validateObj(this.ctx, schema, data, this.errors, options);
         return Object.keys(this.errors).length === 0;
+    }
+
+    reset() {
+        this.errors = {};
     }
 }
 
 export {
-    Validator as default
+    Validator as default,
+    Schema,
+    SchemaInput,
+    SchemaOptions,
+    SchemaType,
+    Options,
+    ValidatorError,
+    Context,
 };
