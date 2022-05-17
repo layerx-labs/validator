52c8b2d2a8e7aabc27fd8132949f0786e5a1928f
AfonsoMatElias <afonsomatumona@hotmail.com>
2022-05-17 14:21:52 +0100

    docs: added commentary to each property

--- types/schema.ts
+++ types/schema.ts
@@ -2,6 +2,12 @@ import SchemaOptions from "./schema-options";
 import SchemaType from "./schema-type";
 
 export default interface Schema {
+	/**
+	 * Defines the type of the property to be validated
+	 */
 	type: SchemaType,
+	/**
+	 * The options of the validation process
+	 */
 	options: SchemaOptions
 }
