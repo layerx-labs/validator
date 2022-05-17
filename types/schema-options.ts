db765ee12195fe57cd748dbaad0bb85b48e2b828
AfonsoMatElias <afonsomatumona@hotmail.com>
2022-05-17 14:20:37 +0100

    docs: added commentary to each property

--- types/schema-options.ts
+++ types/schema-options.ts
@@ -1,22 +1,65 @@
-import SchemaInput from "./schema-input"
 import Schema from "./schema"
+import SchemaFormat from "./schema-format"
+import SchemaInput from "./schema-input"
 
 export default interface SchemaOptions {
+	/**
+	 * The name of the table/entity to get value to validate uniqueness
+	 * 
+	 */
 	orm?: string
+	/**
+	 * Allow the property to be unique to be unique in the table
+	 */
 	unique?: boolean
+	/**
+	 * Allow to apply Regular Expression to the validation process
+	 */
 	regexp?: RegExp
+	/**
+	 * Allow to define maximum size of a property
+	 */
 	max?: number
+	/**
+	 * Allow to define minimum size of a property
+	 */
 	min?: number
+	/**
+	 * Allow to specify what values are accepted
+	 */
 	acceptedValues?: any[]
-	format?: string
+	/**
+	 * Allow to define the format of the `string` type property
+	 */
+	format?: SchemaFormat
+	/**
+	 * Defines a Schema of the `object` type property
+	 */
 	schema?: SchemaInput
+	/**
+	 * Defines a Schema of the `array` object type property
+	 */
 	arrayOf?: SchemaInput
+	/**
+	 * Defines a Schema of the `array` object type property (the same as arrayOf)
+	 */
 	arrayType?: Schema
+	/**
+	 * Allow to define that the property is required
+	 */
 	presence?: boolean
+	/**
+	 * Allow to define minimum size of an array property
+	 */
 	minSize?: number
+	/**
+	 * Allow to define maximum size of an array property
+	 */
 	maxSize?: number
-	validateFuncErrorMsg?: string
-	validateFunc?: (
-		ctx: any, value: any, parent?: object
-	) => Promise<boolean> | boolean,
+	/**
+	 * Allow to apply custom validation
+	 */
+	custom?(
+		ctx: any, value: any, parent?: object, output?: { message: string }
+	): Promise<boolean> | boolean,
 }
